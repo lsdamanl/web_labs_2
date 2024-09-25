40f7d4310805c73d68f5b6d42b666a986456fe07
lsdamanl <shipiloffdima@ya.ru>
2024-09-25 19:04:33 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Шипилов Дмитрий Андреевич, лабораторная работа 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <hl> web-сервер на flask</h1>
+
+        <footer>
+            &copy; Шипилов Дмитрий Андреевич, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
