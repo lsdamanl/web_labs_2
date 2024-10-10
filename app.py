46e04b60c6b7e5de2477ab0fbe4e3a103c2a204e
lsdamanl <shipiloffdima@ya.ru>
2024-10-10 20:54:15 +0700

    Добавление цветка

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def a():
 def a2():
     return 'со слэшем'
 
-flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
 
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
@@ -168,3 +168,18 @@ def flowers(flower_id):
         return "Такого цветка нет, братик", 404
     else:
         return "цветок: " + flower_list[flower_id]
+
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1>Добавлен новый цветок </h1>
+    <p>Название нового цветка: {name} </p>
+    <p>Всего цветов: {len(flower_list)}</p>
+    <p>Полный список: {flower_list}</p>
+    </body>
+</html>
+'''
