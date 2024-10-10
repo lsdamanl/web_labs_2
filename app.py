f2fa8b8c8926464ac47ef0fe249f26716453b37c
lsdamanl <shipiloffdima@ya.ru>
2024-10-10 20:40:08 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -159,3 +159,12 @@ def a():
 @app.route('/lab2/a/')
 def a2():
     return 'со слэшем'
+
+flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        return "Такого цветка нет, братик", 404
+    else:
+        return "цветок: " + flower_list[flower_id]
