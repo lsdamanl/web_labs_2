592da30c67aef1d5f949c17179e43ce190605fd8
lsdamanl <shipiloffdima@ya.ru>
2024-10-11 19:14:18 +0700

    Добавление ссылок

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def menu():
         <nav>
             <ul>
                 <li><a href="/lab1">Первая лабораторная</a></li>
+                <li><a href="/lab2">Вторая лабораторная</a></li>
             </ul>
         </nav>
 
