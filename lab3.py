85fc84395a3a57342e3cde048537692bfbcde684
lsdamanl <shipiloffdima@ya.ru>
2024-10-25 01:18:29 +0700

    Поиск видеокарт по заданному диапазону цен

--- lab3.py
+++ lab3.py
@@ -109,7 +109,7 @@ def settings():
     return resp
 
 
-@lab3.route('/lab3/ticket', methods=['GET', 'POST'])
+@lab3.route('/lab3/ticket', methods=['get', 'post'])
 def ticket():
     if request.method == 'POST':
         name = request.form.get('name')
@@ -145,3 +145,52 @@ def ticket():
                                luggage=luggage, insurance=insurance, ticket_type=ticket_type, price=price)
     
     return render_template('lab3/ticket_form.html')
+
+
+products = [
+    {"name": "NVIDIA GeForce RTX 4090", "price": 250000, "memory": "24 GB", "brand": "NVIDIA"},
+    {"name": "NVIDIA GeForce RTX 4080", "price": 130000, "memory": "16 GB", "brand": "NVIDIA"},
+    {"name": "NVIDIA GeForce RTX 4070 Ti SUPER", "price": 102000, "memory": "12 GB", "brand": "NVIDIA"},
+    {"name": "NVIDIA GeForce RTX 4070", "price": 65000, "memory": "12 GB", "brand": "NVIDIA"},
+    {"name": "NVIDIA GeForce RTX 4060 Ti", "price": 60000, "memory": "8 GB", "brand": "NVIDIA"},
+    {"name": "AMD Radeon RX 7900 XTX", "price": 140000, "memory": "24 GB", "brand": "AMD"},
+    {"name": "AMD Radeon RX 7900 XT", "price": 96000, "memory": "20 GB", "brand": "AMD"},
+    {"name": "AMD Radeon RX 6800 XT", "price": 63000, "memory": "16 GB", "brand": "AMD"},
+    {"name": "AMD Radeon RX 6800", "price": 55000, "memory": "16 GB", "brand": "AMD"},
+    {"name": "NVIDIA GeForce GTX 1660 Super", "price": 23000, "memory": "6 GB", "brand": "NVIDIA"},
+    {"name": "NVIDIA GeForce GTX 1650", "price": 16000, "memory": "4 GB", "brand": "NVIDIA"},
+    {"name": "AMD Radeon RX 6600 XT", "price": 70000, "memory": "8 GB", "brand": "AMD"},
+    {"name": "AMD Radeon RX 6500 XT", "price": 18000, "memory": "4 GB", "brand": "AMD"},
+    {"name": "NVIDIA GeForce RTX 3060 Ti", "price": 37000, "memory": "8 GB", "brand": "NVIDIA"},
+    {"name": "NVIDIA GeForce RTX 3060", "price": 33000, "memory": "12 GB", "brand": "NVIDIA"},
+    {"name": "NVIDIA GeForce RTX 2060", "price": 20000, "memory": "4 GB", "brand": "NVIDIA"},
+    {"name": "AMD Radeon RX 5700 XT", "price": 60000, "memory": "8 GB", "brand": "AMD"},
+    {"name": "AMD Radeon RX 580", "price": 9000, "memory": "8 GB", "brand": "AMD"},
+    {"name": "NVIDIA GeForce GT 1030", "price": 8000, "memory": "2 GB", "brand": "NVIDIA"},
+    {"name": "NVIDIA GeForce GTX 1050 Ti", "price": 6000, "memory": "4 GB", "brand": "NVIDIA"},
+]
+
+@lab3.route('/lab3/search_form')
+def search_form():
+    min_price = request.args.get('min_price') or request.cookies.get('min_price', '0')
+    max_price = request.args.get('max_price') or request.cookies.get('max_price', '250000')
+
+    return render_template('lab3/search_form.html', min_price=min_price, max_price=max_price)
+
+
+@lab3.route('/lab3/search', methods=['get'])
+def search():
+    min_price = request.args.get('min_price', '0')
+    max_price = request.args.get('max_price', '250000')
+
+    min_price = int(min_price)
+    max_price = int(max_price)
+    filtered_products = [product for product in products if min_price <= product["price"] <= max_price]
+
+    resp = make_response(render_template('lab3/results.html', products=filtered_products, min_price=min_price, max_price=max_price))
+    resp.set_cookie('min_price', str(min_price))
+    resp.set_cookie('max_price', str(max_price))
+
+    return resp
+
+    
