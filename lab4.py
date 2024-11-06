ae2c989750f5165d4ff5c21bcc7cf581b9d90ca2
lsdamanl <shipiloffdima@ya.ru>
2024-11-06 21:55:17 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -27,3 +27,69 @@ def div():
 
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/add-form')
+def add_form():
+    return render_template('lab4/add-form.html')
+
+
+@lab4.route('/lab4/add', methods=['POST'])
+def add():
+    x1 = request.form.get('x1') or '0'
+    x2 = request.form.get('x2') or '0'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 + x2
+    return render_template('lab4/add.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/mul-form')
+def mul_form():
+    return render_template('lab4/mul-form.html')
+
+
+@lab4.route('/lab4/mul', methods=['POST'])
+def mul():
+    x1 = request.form.get('x1') or '1'
+    x2 = request.form.get('x2') or '1'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 * x2
+    return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub-form.html')
+
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполнены!')
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/pow-form')
+def pow_form():
+    return render_template('lab4/pow-form.html')
+
+
+@lab4.route('/lab4/pow', methods=['POST'])
+def pow():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/pow.html', error='Оба поля должны быть заполнены!')
+    x1 = int(x1)
+    x2 = int(x2)
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/pow.html', error='0 в степени 0 не определено!')
+    result = x1 ** x2
+    return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
