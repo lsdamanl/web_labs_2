ae4b737259e4090a732ce885d05f86f518d8e716
lsdamanl <shipiloffdima@ya.ru>
2024-12-25 20:34:13 +0700

    Вывод таблицы

--- lab7.py
+++ lab7.py
@@ -4,11 +4,11 @@ lab7 = Blueprint('lab7', __name__)
 
 @lab7.route('/lab7/')
 def main():
-    return render_template('lab7/lab7.html')
+    return render_template('lab7/index.html')
 
 films = [
     {
-        "title": "interstellar",
+        "title": "Interstellar",
         "title_ru": "Интерстеллар",
         "year": 2014,
         "description": "Когда засуха, пыльные бури и вымирание растений приводят\
